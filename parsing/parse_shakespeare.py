7c0127ba1ba852bf2155a57601f06efb56b7b1ef
Jenkins Stephen <Stephen.Jenkins@jhuapl.edu>
2017-11-19 23:03:30 -0500

    Adding initial parsing code for shakespeare along with shakespeare txt

---
+++ parsing/parse_shakespeare.py
@@ -0,0 +1,58 @@
+import re
+
+
+# function for parsing the data
+def data_parser(text, dic):
+    for i, j in dic.iteritems():
+        text = re.sub(i, j, text)
+    return text
+
+# Files to read and write to
+input_file_name = '../data/shakespeare_raw.txt'
+output_file_name = 'shakespeare.txt'
+
+# Read file
+input_file = open(input_file_name)
+my_text = input_file.readlines()[:] # Read the whole text file,
+input_file.close()
+
+regexs = {'-':'',
+    '\[(.*?)\]':'',
+    '\[(.*?)':'',
+    '(.*?)\]':'',
+    '\((.*?)\)':'',
+    '  +':' ',
+    '^\t':'',
+    '\t':' ',
+    'SCENE.*':'',
+    'ACT.*':'',
+    '^[0-9].*':'', # Lines beginning with numeric char
+    '.*\|.*':'', # Weird lines with | character
+    '^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})\..*':'' # Roman Numerals
+}
+
+output_file = open(output_file_name, 'w')
+
+for line in my_text:
+    line = data_parser(line, regexs)
+    # line = line.replace('-', '')
+    # line = re.sub('\[(.*?)\]', '', line)
+    # line = re.sub('\[(.*?)', '', line)
+    # line = re.sub('(.*?)\]', '', line)
+    # line = re.sub('\((.*?)\)', '', line)
+    # line = re.sub('  +', ' ', line)
+    # line = re.sub('^\t', '', line)
+    # line = re.sub('\t', ' ', line)
+    # line = re.sub('SCENE.*', '', line)
+    # line = re.sub('ACT.*', '', line)
+    # line = re.sub('^[0-9].*', '', line) # Lines that start with numeric char
+    # line = re.sub('.*\|.*', '', line) # Replace weird lines with | character
+    # line = re.sub('^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})\..*', '', line) # Replace Roman Numerals
+
+    if line != '\n': # If line hasn't been reduced to empty line, write it out
+        output_file.write(line)
+
+output_file.close()
+
+
+
