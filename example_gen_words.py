4f41621306cdd292a8f92bc7a7a1cb470d78610e
Leon He <Leon.He@jhuapl.edu>
2017-11-23 16:43:36 -0500

    uncommenting

--- example_gen_words.py
+++ example_gen_words.py
@@ -56,7 +56,7 @@ model.add(LSTM(256))
 model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation='softmax'))
 # load the network weights
-filename = 'weights-improvement-20-2.0762-larger.hdf5'
+filename = 'words-weights-improvement-20-2.0762-larger.hdf5'
 model.load_weights(filename)
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 # pick a random seed
--- example_words.py
+++ example_words.py
@@ -47,19 +47,19 @@ X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
 X = X / float(n_vocab)
 
 
-# # one hot encode the output variable
-# y = np_utils.to_categorical(dataY)
-# # define the LSTM model
-# model = Sequential()
-# model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
-# model.add(Dropout(0.2))
-# model.add(LSTM(256))
-# model.add(Dropout(0.2))
-# model.add(Dense(y.shape[1], activation='softmax'))
-# model.compile(loss='categorical_crossentropy', optimizer='adam')
-# # define the checkpoint
-# filepath='weights-improvement-{epoch:02d}-{loss:.4f}-larger.hdf5'
-# checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
-# callbacks_list = [checkpoint]
-# # fit the model
-# model.fit(X, y, epochs=20, batch_size=128, callbacks=callbacks_list)
+# one hot encode the output variable
+y = np_utils.to_categorical(dataY)
+# define the LSTM model
+model = Sequential()
+model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
+model.add(Dropout(0.2))
+model.add(LSTM(256))
+model.add(Dropout(0.2))
+model.add(Dense(y.shape[1], activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+# define the checkpoint
+filepath='words-weights-improvement-{epoch:02d}-{loss:.4f}-larger.hdf5'
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+callbacks_list = [checkpoint]
+# fit the model
+model.fit(X, y, epochs=20, batch_size=128, callbacks=callbacks_list)
